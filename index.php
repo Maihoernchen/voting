1a38b5c3642dd73892f118df800c0498c58c6f09
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2023-11-25 14:13:32 +0100

    enter

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?
 
-echo '<a href="./login/login.php">Login</a>'
-
+if (!$_SESSION['iserv'] AND !$_COOKIE['iserv']) {
+echo '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <form action="login/logi.php" method="post">
+        <input type="text" name="iserv" placeholder="ISERV-Mail-Adresse" required><br>
+        <button type="submit" name="submit">Log In</button>
+    </form>
+  <a href="register.php">Register</a>
+  </body>
+</html>';
+}
 ?>
